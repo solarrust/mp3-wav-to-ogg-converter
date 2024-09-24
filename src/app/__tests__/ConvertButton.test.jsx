b7b18fda2407205881cddad97c70a61ff7705053
Alena Batitskaia <batickaya.a@gmail.com>
2024-09-24 13:48:46 +0100

    ConvertButton tests

---
+++ src/app/__tests__/ConvertButton.test.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import ConvertButton from "../components/ConvertButton";
+
+describe("ConvertButton Component", () => {
+  it("calls onClick function when clicked", () => {
+    const handleClick = vi.fn();
+    const { getByText } = render(<ConvertButton onClick={handleClick} />);
+
+    const button = getByText("Convert into OGG");
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
