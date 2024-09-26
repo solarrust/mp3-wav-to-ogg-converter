cfbffc18628b3cd2749556fc7cb57944fd79f4a2
Alena Batitskaia <batickaya.a@gmail.com>
2024-09-26 11:55:15 +0100

    Catch errors in wasm loading, show error in UI

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,23 +1,37 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import loadWasm from "../lib/loadWasm";
 import { Typography } from "@mui/material";
-import Converter from "./Converter"
+import Converter from "./Converter";
 
+// TODO: написать тесты на компонент
+// TODO: показывать ошибки в интерфейсе (во всех компонентах). Где есть промисы и асинк/авейт — обрабатывать обязательно
+// TODO: Поправить именование классов внутри компонентов
 export default function App() {
-  const [loaded, setLoaded] = useState(false);
-  const ffmpegRef = useRef(null);
+  const [ffmpeg, setFfmpeg] = useState(null);
+  const [error, setError] = useState({ isError: false, message: "" });
 
   const loadFfmpeg = async () => {
-    ffmpegRef.current = await loadWasm();
-    setLoaded(true);
+    let ffmpeg;
+
+    try {
+      ffmpeg = await loadWasm();
+    } catch (er) {
+      setError({ isError: true, message: er.message });
+    }
+
+    setFfmpeg(ffmpeg);
   };
 
   useEffect(() => {
     loadFfmpeg();
   }, []);
 
-  return loaded ? (
-    <Converter ffmpeg={ffmpegRef.current} />
+  return ffmpeg ? (
+    <Converter ffmpeg={ffmpeg} />
+  ) : error.isError ? (
+    <Typography className="converter__error" variant="subtitle1">
+      {error.message}
+    </Typography>
   ) : (
     <Typography className="converter__preview" variant="subtitle1">
       Launching the system 🚀
--- src/app/lib/loadWasm.js
+++ src/app/lib/loadWasm.js
@@ -1,13 +1,17 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 
-async function loadWasm() {
+export default async function loadWasm() {
   const start = performance.now();
   const ffmpeg = new FFmpeg()
-  await ffmpeg.load();
+
+  try {
+    await ffmpeg.load();
+  } catch (error) {
+    throw error
+  }
 
   const timeConsumed = performance.now() - start;
   console.log(`Time for loading ffmpeg.wasm: ${timeConsumed}`);
+
   return ffmpeg;
 }
-
-export default loadWasm;
